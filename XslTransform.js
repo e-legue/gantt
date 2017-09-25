eb4d318109e927699279d866fb7715717c915dc1
Legue <emmanuel.legue@kronos.com>
2017-09-25 18:07:45 -0400

    [WIP] add xml station file templates.

---
+++ XslTransform.js
@@ -0,0 +1,68 @@
+dojo.provide("XslTransform");
+
+dojo.declare("XslTransform", [],
+{
+	_xslDoc : null,
+	_xslPath : null,
+
+	constructor : function(xslPath) {
+		this._xslPath = xslPath;
+	},
+
+	transform : function(xmlPath) {
+		if (this._xslDoc === null)
+			this._xslDoc = this._loadXML(this._xslPath);
+
+		var result = null;
+		var xmlDoc = this._loadXML(xmlPath);
+
+		if (dojo.isIE) {
+			result = xmlDoc.transformNode(this._xslDoc);
+		} else if(typeof XSLTProcessor !== undefined) {
+			xsltProcessor = new XSLTProcessor();
+	  		xsltProcessor.importStylesheet(this._xslDoc);
+
+	  		var ownerDocument = document.implementation.createDocument("", "", null);
+	  		result = xsltProcessor.transformToFragment(xmlDoc, ownerDocument);
+		} else {
+			alert("Your browser doesn't support XSLT!");
+		}
+
+		return result;
+
+	},
+
+	createXMLDocument : function(xmlText) {
+		var xmlDoc = null;
+
+		if (dojo.isIE) {
+			xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+			xmlDoc.async = false;
+			xmlDoc.loadXML(xmlText);
+		} else if (window.DOMParser) {
+			parser = new DOMParser();
+			xmlDoc = parser.parseFromString(xmlText, "text/xml");
+		} else {
+			alert("Your browser doesn't suppoprt XML parsing!");
+		}
+
+		return xmlDoc;
+	},
+
+	// Synchronously loads a remote xml file
+	_loadXML : function(xmlPath) {
+		var getResult = dojo.xhrGet({
+			url : xmlPath,
+			handleAs : "xml",
+			sync: true
+		});
+
+		var xml = null;
+		// Returns immediately, because the GET is synchronous.
+		var xmlData = getResult.then(function (response) {
+			xml = response;
+		});
+
+		return xml;
+	}
+});
